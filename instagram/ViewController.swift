138684428b28cbdcefaad7bef432ceea58b12c2d
Imam Abdul Azis <imamabdulazis028@gmail.com>
2020-11-09 00:23:41 +0700

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Varx on 11/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
