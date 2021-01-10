91025a1be4ded490d4bd5604922edb17588ec156
imamabdulazis <imamabdulazis028@gmail.com>
2021-01-10 13:23:47 +0700

    add Podfile

---
+++ instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  instagram
+//
+//  Created by Varx on 1/6/21.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ instagram/Controllers/Other/PublishPostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PublishPostViewController.swift
+//  instagram
+//
+//  Created by Varx on 1/6/21.
+//
+
+import UIKit
+
+class PublishPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
